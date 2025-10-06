8fb517777b25a0bcbfea39e6c4eb0e5dfd67701d
Osekobo <collinsboseko2005@gmail.com>
2025-10-06 14:26:19 +0300

    final_project

--- database.py
+++ database.py
@@ -7,13 +7,10 @@ conn = psycopg2.connect(user="postgres", password="12039",
 cur = conn.cursor()
 
 # getting products
-
-
 def get_products():
     cur.execute("select * from products")
     products = cur.fetchall()
     return products
-
 
 # getting  sales
 def get_sales():
@@ -21,13 +18,11 @@ def get_sales():
     sales = cur.fetchall()
     return sales
 
-
 # getting stock
 def get_stock():
     cur.execute("select * from stock")
     stock = cur.fetchall()
     return stock
-
 
 # method 1 inserting products
 def insert_products(values):
@@ -35,21 +30,17 @@ def insert_products(values):
     cur.execute(insert_products, values)
     conn.commit()
 
-# current_datetime = str(datetime.now())
-
 # inserting stock
 def insert_stock(values):
     insert_stock = "insert into stock(pid,stock_quantity,created_at)values(%s,%s,now())"
     cur.execute(insert_stock, values)
     conn.commit()
 
-
 # inserting sale
 def insert_sales(values):
     insert_sales = "insert into sales(pid,quantity,created_at)values(%s,%s,now())"
     cur.execute(insert_sales, values)
     conn.commit()
-
 
 # sales per products
 def sales_per_product():
@@ -61,8 +52,6 @@ def sales_per_product():
     return sales_product
 
 # sales per day
-
-
 def sales_per_day():
     cur.execute("""
         select date(sales.created_at) as date , sum(sales.quantity * products.selling_price) from sales join
@@ -72,8 +61,6 @@ def sales_per_day():
     return sales_day
 
 # profit per product
-
-
 def profit_per_product():
     cur.execute("""
         select products.name as p_name , sum(sales.quantity * (products.selling_price - products.buying_price))
@@ -83,8 +70,6 @@ def profit_per_product():
     return profit_product
 
 # profit per day
-
-
 def profit_per_day():
     cur.execute("""
         select date(sales.created_at) as date , sum(sales.quantity * (products.selling_price - products.buying_price))
@@ -93,14 +78,12 @@ def profit_per_day():
     profit_day = cur.fetchall()
     return profit_day
 
-
 def available_stock(pid):
     cur.execute(
         "select sum(stock.stock_quantity)from stock where pid = %s", (pid,))
     total_stock = cur.fetchone()[0] or 0
     cur.execute("select sum(sales.quantity)from sales where pid = %s", (pid,))
     total_sold = cur.fetchone()[0] or 0
-
     return total_stock - total_sold
 
 # checking if user exists
@@ -113,13 +96,9 @@ def insert_user(user_details):
     cur.execute("insert into users(full_name,email,phone_number,password)values(%s,%s,%s,%s)",(user_details))
     conn.commit()
 
-
-
 #update edited product
 def edited_product(x):
     cur.execute("update products set name = %s, buying_price = %s, selling_price = %s where id =%s",x)
     conn.commit()
-
-
-
+    
 sales_product = sales_per_product()
